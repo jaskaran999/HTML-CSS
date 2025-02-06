75959f43c31496b33f8b15ac5a2519b35cf32ffe
jaskaran999 <jaskaran.singh9807@gmail.com>
2025-02-06 18:55:50 +0530

    Delete final_project/script.js

--- final_project/script.js
+++
@@ -1,7 +0,0 @@
-const bar = document.getElementById("bar");
-const nav = document.getElementById("navbar");
-if (bar) {
-    bar.addEventListener('click',() => {
-            nav.classlist.add('active');
-    })
-}
