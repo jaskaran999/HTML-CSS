afc81d1cc1779f0adfba7cfaa2cd382b998fbaf5
jaskaran999 <jaskaran.singh9807@gmail.com>
2025-02-06 17:56:25 +0530

    Add files via upload

---
+++ final_project/script.js
@@ -0,0 +1,7 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+if (bar) {
+    bar.addEventListener('click',() => {
+            nav.classlist.add('active');
+    })
+}
